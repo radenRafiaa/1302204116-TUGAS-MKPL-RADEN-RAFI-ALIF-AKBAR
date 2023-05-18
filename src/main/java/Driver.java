f18f148d12ee5769475bb3385c51795b6619b909
RAFI ALIF <rafialif2907@gmail.com>
2023-05-18 23:22:09 +0700

    kasus test berhasil

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,19 +3,22 @@ import src.main.java.Counter;
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
 		Counter counter = new Counter();
-		
+
 		System.out.println("Count:" + counter.getCount());
-		
+		System.out.println("Current cout:" + counter.getCount());
+
 		counter.increment();
-		
+
 		System.out.println("Count:" + counter.getCount());
-		
+		System.out.println("Current cout:" + counter.getCount());
+
 		counter.decrement();
-		
+
 		System.out.println("Count:" + counter.getCount());
-		
+		System.out.println("Current cout:" + counter.getCount());
+
 	}
 
 }
