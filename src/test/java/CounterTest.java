72675e8210bd11c3c16db6e1931073baa5679272
RAFI ALIF <rafialif2907@gmail.com>
2023-05-24 21:53:56 +0700

    fix bug

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,4 +1,7 @@
+package src.test.java;
+
 import static org.junit.Assert.*;
+import src.main.java.*;
 import org.junit.Test;
 
 public class CounterTest {
@@ -6,21 +9,21 @@ public class CounterTest {
 	@Test
 	public void testReset() {
 		Counter testCounter = new Counter();
-
-		for (int i = 0; i < 10; i++) {
+		
+		for(int i = 0; i <10; i++) {
 			testCounter.increment();
 		}
-
+		
 		testCounter.reset();
-
+		
 		assertEquals(testCounter.getCount(), 0);
 	}
 
 	@Test
 	public void testIncrement() {
 		Counter testCounter = new Counter();
-
-		for (int i = 1; i < 10; i++) {
+		
+		for(int i = 1; i <10; i++) {
 			testCounter.increment();
 			assertEquals(testCounter.getCount(), i);
 		}
@@ -29,8 +32,8 @@ public class CounterTest {
 	@Test
 	public void testDecrement() {
 		Counter testCounter = new Counter();
-
-		for (int i = 1; i < 10; i++) {
+		
+		for(int i = 1; i <10; i++) {
 			testCounter.decrement();
 			assertEquals(testCounter.getCount(), i * -1);
 		}
